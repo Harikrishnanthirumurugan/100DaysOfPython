091f0bb5cb64eb7c2c3b48fc6d70f2cdfda6acdb
HARIKRISHNAN T <134075171+Harikrishnanthirumurugan@users.noreply.github.com>
2024-04-14 12:10:48 +0530

    Completed Day 1 of 100 Days of Python course
    In this Project i learn about basic String Operations and Variable declarations in Python

---
+++ Day-1-Project.py
@@ -0,0 +1,6 @@
+#getting Values from the user
+
+print("Welcome to the Band Name Generator Created with Py")
+a=input("What is your Pet Name : ")
+b=input("What is the name of your place : ")
+print("Your Brand Name could be : " +a + " " + b)
